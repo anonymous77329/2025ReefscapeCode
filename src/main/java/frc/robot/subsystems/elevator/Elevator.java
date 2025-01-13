186fbb718e9a1defc36d968d955cab2f12814df1
sag13 <sagizar444@gmail.com>
2025-01-13 19:12:17 +0200

    feat: Add a simulation for the Elevator

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -1,10 +1,13 @@
 package frc.robot.subsystems.elevator;
 
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.generic.hardware.motor.MotorProperties;
+import org.littletonrobotics.junction.Logger;
 
 import static frc.robot.subsystems.elevator.ElevatorConstants.ELEVATOR_LEFT_MOTOR;
 import static frc.robot.subsystems.elevator.ElevatorConstants.ELEVATOR_RIGHT_MOTOR;
@@ -24,6 +27,12 @@ public class Elevator extends SubsystemBase {
                 this
         );
     }
+
+    public void periodic() {
+        Pose3d current3DPose = new Pose3d(0, 0, getCurrentPosition(), new Rotation3d(0, 0, 0));
+        Logger.recordOutput("Elevator", current3DPose);
+    }
+
 // need to add if statement so that the elevator won't brake while I press and to start
 // the motors and the elevator is in the end
     private void setMotorVoltage(boolean isInverted) {
@@ -33,4 +42,7 @@ public class Elevator extends SubsystemBase {
         ELEVATOR_RIGHT_MOTOR.setOutput(MotorProperties.ControlMode.VOLTAGE, targetVoltage);
     }
 
+    private double getCurrentPosition() {
+        return ELEVATOR_LEFT_MOTOR.getSystemPosition();
+    }
 }
