78871d527f00ae18af7444ba898bea8179fb62b5
sag13 <sagizar444@gmail.com>
2025-01-15 20:20:39 +0200

    Refactor: Variables that should be final have changed, better names, etc...

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -4,48 +4,40 @@ import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.generic.GenericSubsystem;
 import frc.lib.generic.characterization.StaticFrictionCharacterization;
 import frc.lib.generic.hardware.motor.MotorProperties;
-import frc.lib.generic.visualization.mechanisms.ElevatorMechanism2d;
+import frc.lib.math.Conversions;
 import org.littletonrobotics.junction.Logger;
 
 import static frc.robot.subsystems.elevator.ElevatorConstants.*;
 
 public class Elevator extends GenericSubsystem {
-    private final ElevatorMechanism2d mechanism = new ElevatorMechanism2d("Elevator Mechanism", 1);
-    private final StaticFrictionCharacterization frictionCheck = new StaticFrictionCharacterization(this, ELEVATOR_LEFT_MOTOR, 0);
-
     public Command setTargetPosition(double targetPositionMeters){
         return Commands.runEnd(() -> {
-            setMotorPosition(metersToRotations(targetPositionMeters));
-            printPose(targetPositionMeters);  // prints the elevator 3DPose
+            setMotorPosition(Conversions.metresToRotations(targetPositionMeters, WHEEL_DIAMETER));
+            printPose(targetPositionMeters);
         }, this::stopMotors, this);
     }
 
     public void periodic() {
-//        if (TOP_BEAM_BREAKER.get() == 0) {
-//            ELEVATOR_LEFT_MOTOR.setMotorEncoderPosition(0);
-//            ELEVATOR_RIGHT_MOTOR.setMotorEncoderPosition(0);
-//        }
-//
-//        if (TOP_BEAM_BREAKER.get() == TOP_ELEVATOR_METERS_POSITION) {
-//            ELEVATOR_LEFT_MOTOR.setMotorEncoderPosition(TOP_ELEVATOR_METERS_POSITION);
-//            ELEVATOR_RIGHT_MOTOR.setMotorEncoderPosition(TOP_ELEVATOR_METERS_POSITION);
-//        }
-    }
+        if (TOP_BEAM_BREAK.get() == 0) {
+            ELEVATOR_LEFT_MOTOR.setMotorEncoderPosition(0);
+            ELEVATOR_RIGHT_MOTOR.setMotorEncoderPosition(0);
+        }
 
-    public Command giveMeThefrictionCheckYoooo() {
-        return frictionCheck;
+        if (TOP_BEAM_BREAK.get() == ELEVATOR_MAX_EXTENSION_METERS) {
+            ELEVATOR_LEFT_MOTOR.setMotorEncoderPosition(ELEVATOR_MAX_EXTENSION_METERS);
+            ELEVATOR_RIGHT_MOTOR.setMotorEncoderPosition(ELEVATOR_MAX_EXTENSION_METERS);
+        }
     }
 
     private void printPose(double targetPositionMetes) {
-        Pose3d current3DPose = new Pose3d(0, 0, targetPositionMetes, new Rotation3d(0, 0, 0));
+        final Pose3d current3DPose = new Pose3d(0, 0, targetPositionMetes, new Rotation3d(0, 0, 0));
         Logger.recordOutput("Elevator", current3DPose);
-        mechanism.updateCurrentPosition(ELEVATOR_LEFT_MOTOR.getSystemPosition());
-        mechanism.updateTargetPosition(targetPositionMetes);
-        Logger.recordOutput("Elevator works??", ELEVATOR_LEFT_MOTOR.getSystemPosition());
+
+        MECHANISM.updateCurrentPosition(ELEVATOR_LEFT_MOTOR.getSystemPosition());
+        MECHANISM.updateTargetPosition(targetPositionMetes);
     }
 
     private void setMotorPosition(double targetPosition) {
@@ -56,12 +48,5 @@ public class Elevator extends GenericSubsystem {
     private void stopMotors() {
         ELEVATOR_LEFT_MOTOR.stopMotor();
         ELEVATOR_RIGHT_MOTOR.stopMotor();
-    }
-
-    private double metersToRotations(double meters) {
-        double pulleyDiameter = 0.05;
-        double pulleyCircumference = Math.PI * pulleyDiameter;
-
-        return meters / pulleyCircumference;
     }
 }
--- src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorConstants.java
@@ -5,6 +5,7 @@ import frc.lib.generic.hardware.motor.*;
 import frc.lib.generic.hardware.sensors.Sensor;
 import frc.lib.generic.hardware.sensors.SensorFactory;
 import frc.lib.generic.simulation.SimulationProperties;
+import frc.lib.generic.visualization.mechanisms.ElevatorMechanism2d;
 
 public class ElevatorConstants {
     protected static final Motor
@@ -12,24 +13,28 @@ public class ElevatorConstants {
             ELEVATOR_RIGHT_MOTOR = MotorFactory.createSpark("Elevator Right Motor", 1, MotorProperties.SparkType.MAX);
 
     protected static final Sensor
-            TOP_BEAM_BREAKER = SensorFactory.createDigitalInput("Top Beam Breaker", 2),
-            BUTTON_BEAM_BREAKER = SensorFactory.createDigitalInput("Button Beam Breaker", 3);
-
-    protected static final double TOP_ELEVATOR_METERS_POSITION = 1;
+            TOP_BEAM_BREAK = SensorFactory.createDigitalInput("Top Beam Breaker", 2),
+            BOTTOM_BEAM_BREAK = SensorFactory.createDigitalInput("Button Beam Breaker", 3);
 
     private static final MotorConfiguration ELEVATOR_LEFT_MOTOR_CONFIGURATION = new MotorConfiguration();
+
+    protected static final double
+            ELEVATOR_MAX_EXTENSION_METERS = 0.86,
+            WHEEL_DIAMETER = 0.05,
+            WHEEL_SCOPE = Math.PI * WHEEL_DIAMETER;
+
+    protected static final ElevatorMechanism2d MECHANISM = new ElevatorMechanism2d("Elevator Mechanism", 0.8);
 
     static {
         configureMotorConfiguration();
     }
 
     private static void configureMotorConfiguration() {
-
         ELEVATOR_LEFT_MOTOR_CONFIGURATION.idleMode = MotorProperties.IdleMode.BRAKE;
-        ELEVATOR_LEFT_MOTOR_CONFIGURATION.simulationSlot = new MotorProperties.Slot(70, 0, 0, 0, 0, 1.3129999999999662);
+        ELEVATOR_LEFT_MOTOR_CONFIGURATION.simulationSlot = new MotorProperties.Slot(70, 0, 0, 0, 0,  1.313);
 
-        ELEVATOR_LEFT_MOTOR_CONFIGURATION.profileMaxAcceleration = 15;
         ELEVATOR_LEFT_MOTOR_CONFIGURATION.profileMaxVelocity = 10;
+        ELEVATOR_LEFT_MOTOR_CONFIGURATION.profileMaxAcceleration = 15;
         ELEVATOR_LEFT_MOTOR_CONFIGURATION.profileMaxJerk = 150;
 
         ELEVATOR_LEFT_MOTOR_CONFIGURATION.simulationProperties = new SimulationProperties.Slot(
